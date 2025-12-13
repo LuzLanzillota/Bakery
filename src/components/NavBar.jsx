f21ba0477b7968b632479f78af5e6cbd0c66b431
FedeZagogo <federica.zago@davinci.edu.ar>
2025-12-13 13:32:06 -0300

    menu hamburguesa

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,48 +2,40 @@ import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import "./NavBar.css";
 
-
 function NavBar() {
-const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-return (
-<header className="navbar-container">
-<div className="logo-container">
-<Link to="/">
-<img
-src="/images/Bakery-ecommerce-1 1.png"
-alt="Logo de Bakery"
-className="logo"
-/>
-</Link>
-</div>
+  return (
+    <header className="navbar-container">
+      <div className="logo-container">
+        <Link to="/">
+          <img
+            src="/images/Bakery-ecommerce-1 1.png"
+            alt="Logo de Bakery"
+            className="logo"
+          />
+        </Link>
+      </div>
 
+      {/* Botón hamburguesa */}
+      <button className="hamburger" onClick={toggleMenu}>
+        <span className="material-icons">dehaze</span>
+      </button>
 
-{/* Ícono de menú hamburguesa */}
-<button
-className="hamburger-button"
-onClick={() => setIsOpen(!isOpen)}
->
-<img
-src="/images/icono-hamburguesa.png" /* reemplazar con la imagen real */
-alt="Menú"
-className="hamburger-icon"
-/>
-</button>
-
-
-<nav className={`navbar ${isOpen ? "open" : ""}`}>
-<ul className="nav-links">
-<li><NavLink to="/" onClick={() => setIsOpen(false)}>Inicio</NavLink></li>
-<li><NavLink to="/productos" onClick={() => setIsOpen(false)}>Productos</NavLink></li>
-<li><NavLink to="/sucursales" onClick={() => setIsOpen(false)}>Sucursales</NavLink></li>
-<li><NavLink to="/novedades" onClick={() => setIsOpen(false)}>Novedades</NavLink></li>
-</ul>
-</nav>
-</header>
-);
+      <nav className={`navbar ${isOpen ? "open" : ""}`}>
+        <ul className="nav-links">
+          <li><NavLink to="/" onClick={() => setIsOpen(false)}>Inicio</NavLink></li>
+          <li><NavLink to="/productos" onClick={() => setIsOpen(false)}>Productos</NavLink></li>
+          <li><NavLink to="/sucursales" onClick={() => setIsOpen(false)}>Sucursales</NavLink></li>
+          <li><NavLink to="/novedades" onClick={() => setIsOpen(false)}>Novedades</NavLink></li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
-
 
 export default NavBar;
